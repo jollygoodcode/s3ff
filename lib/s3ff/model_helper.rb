32b0858fd21aeb8a7b72b0d22d550483424b980a
choonkeat <choonkeat@gmail.com>
2014-11-28 14:25:19 +0800

    Allow download_from_direct_url_with_delay to specify if we want to set _direct_url to nil after processing - also, detect and work with "_direct_url" column if it exists, for sidekiq users

--- lib/s3ff/model_helper.rb
+++ lib/s3ff/model_helper.rb
@@ -1,19 +1,34 @@
 module S3FF
   module ModelHelper
-    def download_from_direct_url_with_delay(attr_name)
+    def download_from_direct_url_with_delay(attr_name, nil_direct_url_after_save: false)
       if self.respond_to?(:delay)
+        has_db_column = self.column_names.include?("#{attr_name}_direct_url") rescue false
+
+        if has_db_column
+          self.class_eval <<-EOM
+
+            after_save :delay_s3ff_download_direct_url, if: -> { #{attr_name}_direct_url.present? && #{attr_name}_direct_url_changed? }
+
+          EOM
+        else
+          self.class_eval <<-EOM
+
+            def #{attr_name}_direct_url
+              @#{attr_name}_direct_url
+            end
+
+            def #{attr_name}_direct_url=(val)
+              #{"return if val.blank?" if nil_direct_url_after_save}
+              self.updated_at = Time.now if val != @#{attr_name}_direct_url
+              @#{attr_name}_direct_url = val
+            end
+
+            after_save :delay_s3ff_download_direct_url, if: proc { #{attr_name}_direct_url.present? }
+
+          EOM
+        end
+
         self.class_eval <<-EOM
-
-          def #{attr_name}_direct_url
-            @#{attr_name}_direct_url
-          end
-
-          def #{attr_name}_direct_url=(val)
-            self.updated_at = Time.now if val != @#{attr_name}_direct_url
-            @#{attr_name}_direct_url = val
-          end
-
-          after_save :delay_s3ff_download_direct_url, if: proc { #{attr_name}_direct_url.present? }
 
           def delay_s3ff_download_direct_url
             self.class.delay.s3ff_download_direct_url(id, #{attr_name}_direct_url)
@@ -24,11 +39,14 @@ module S3FF
               find(instance_id).update(
                 #{attr_name}: file,
                 #{attr_name}_file_name: File.basename(#{attr_name}_direct_url),
+                #{"#{attr_name}_direct_url: nil," if nil_direct_url_after_save}
               )
             end
           end
         EOM
       elsif self.respond_to?(:handle_asynchronously)
+
+        # handle_asynchronously requires db column anyways
         self.class_eval <<-EOM
           after_save :s3ff_download_direct_url, if: -> { #{attr_name}_direct_url.present? && #{attr_name}_direct_url_changed? }
 
@@ -37,6 +55,7 @@ module S3FF
               update(
                 #{attr_name}: file,
                 #{attr_name}_file_name: File.basename(#{attr_name}_direct_url),
+                #{"#{attr_name}_direct_url: nil," if nil_direct_url_after_save}
               )
             end
           end
